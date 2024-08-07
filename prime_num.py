fadecdee2baf2fad6f3340ee30d78b584c3cea92
drishti252 <150322789+drishti252@users.noreply.github.com>
2024-08-07 18:07:35 +0530

    Add files via upload

---
+++ prime_num.py
@@ -0,0 +1,17 @@
+def isprime(n):
+    if n > 1:
+        for i in range(2, (n//2)+1):
+            if (n % i) == 0:
+                print(n, "is not a prime number")
+                break
+        else:
+            print(n, "is a prime number")
+    else:
+        print(n, "is not a prime number")
+
+n=int(input("Enter any number: "))
+isprime(n)
+
+
+
+
