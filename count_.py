a7d16a41030272e85df1e41da2bd0e46c7d8e59a
drishti252 <150322789+drishti252@users.noreply.github.com>
2024-08-07 18:36:25 +0530

    count_.py

--- count_.py
+++ count_.py
@@ -2,16 +2,14 @@ def count(s):
     vowels = 'aeiouAEIOU'
     num_vowels = 0
     num_consonants = 0
-    for char in s:
-        if char.isalpha():  
-            if char in vowels:
+    for ch in s:
+        if ch.isalpha():  
+            if ch in vowels:
                 num_vowels += 1
             else:
                 num_consonants += 1
-                
     return num_vowels, num_consonants
 string = input("Enter a string: ")
 vowels, consonants = count(string)
-print(f"Number of vowels: {vowels}")
-print(f"Number of consonants: {consonants}")
-
+print("Number of vowels:" vowels)
+print("Number of consonants:" consonants)
